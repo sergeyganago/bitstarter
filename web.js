d51783ff0639544f7da2006ec6c0cf793653a7dd
Sergey Ganago <sergey.ganago@gmail.com>
2013-07-10 08:09:57 +0000

    updated web.js

--- web.js
+++ web.js
@@ -2,11 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var text = "";
+var infile = "index.html";
+text = fs.readFileSync(infile);
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
-
-var port = process.env.PORT || 5000;
+  response.send(text);
+}
+ port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
